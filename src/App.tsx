cb877d172d77884d4580f2525980a04d7664c947
MaksimCeleron <143294016+MaksimCeleron@users.noreply.github.com>
2025-08-06 10:41:22 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import logo from "./assets/logo.jpg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -15,21 +16,28 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <a
+          href="https://github.com/MaksimCeleron/Em-13-Front-HW-5"
+          target="_blank"
+        >
+          <img src={logo} className="logo" alt="MaksimCeleron logo" />
+        </a>
       </div>
+      <h1>Emolod 13 Front HW 5</h1>
       <h1>Vite + React</h1>
       <div className="card">
+        <h2>Вбудований клікер у шаблон Vite</h2>
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Кліків: {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Натисніть на лого Vite та React щоб дізнатись більше
+        <br />
+        або на моє лого щоб потрапити в репозиторій
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
